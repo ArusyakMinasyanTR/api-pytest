d710e225b52ba5002d19d8e57c418c7b814071d8
arusyakmin <arusyak.minassian@gmail.com>
2026-01-16 19:00:40 +0400

    add some changes

--- test-login-selenium.py
+++ test-login-selenium.py
@@ -5,17 +5,13 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 def test_valid_login():
-    # Set up the Chrome driver with Service
     
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     driver.get("https://www.saucedemo.com/")
 
-    # Enter username
     driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    # Enter password
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    # Click login
     driver.find_element(By.ID, "login-button").click()
 
     # Assert successful login by checking for presence of inventory page
